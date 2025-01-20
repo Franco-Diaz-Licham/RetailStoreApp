0f155ca6c0623255bc591ff6e2e847bdc9ff26fb
smartdevau@outlook.com <franco.diazlicham@hotmail.com>
2025-01-20 22:22:04 +1100

    # Added Unit of work implementation

--- RetailStore.Server/Data/IUnitOfWork.cs
+++ RetailStore.Server/Data/IUnitOfWork.cs
@@ -2,5 +2,6 @@ namespace RetailStore.Server.Data;
 
 public interface IUnitOfWork
 {
-    
+    IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+    Task<int> Complete();
 }
